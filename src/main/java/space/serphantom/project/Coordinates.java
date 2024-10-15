c3de4828de91de23a0932c3a138487ef8f1d64e5
Lebedev Sergey <sergeylebedevvach@gmail.com>
2024-10-15 20:42:44 +0700

    Main: Add Coordinates.java (Coordinates for Post.java)

---
+++ src/main/java/space/serphantom/project/Coordinates.java
@@ -0,0 +1,32 @@
+package space.serphantom.project;
+
+public class Coordinates {
+    private double lat;
+    private double lon;
+
+    public Coordinates(double lat, double lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates:{ lat: " + this.lat + ", lon: " + this.lon + " }";
+    }
+}
